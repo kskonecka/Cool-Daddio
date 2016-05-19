c51118056182e923c0bab375356cd938ba18e8e5
Kasia Skonecka <kasia.skonecka@gmail.com>
2016-05-19 21:50:30 +0200

    gallery and scrolls

--- js/app.js
+++ js/app.js
@@ -22,6 +22,26 @@ $(function(){
 
 watchTrailer();
 
+function supportUs() {
+  var supportLink = $('.a-support');
+
+
+  supportLink.on('click', function(){
+    // var trailerUrl = "https://www.youtube.com/embed/gK_GQdiXTCc?rel=0&amp;showinfo=0";
+    var support = $('.sup-div');
+    var fullScreen = $('<div>'); // create div that contains trailer
+
+    fullScreen.addClass('fullScreen').fadeIn('slow').appendTo($('.support-screen'));//add fullScreen class and attach to <body>
+    support.removeClass("invisible").appendTo(fullScreen);//add img src and attach to <fullScreen>
+
+    $('body').on('click', '.fullScreen', function(){ //exits full screen
+      fullScreen.fadeOut('slow');
+    });
+  });//end of trailerLink.event
+}
+
+supportUs();
+
 function scrollMenu(){
     var menuLinks = $(".menu").find("a");
     // console.log(menuLinks);
